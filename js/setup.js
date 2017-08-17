0d9a855259e0827ff008a654c524e25231d49b6a
wubabka <wubabca@gmail.comm>
2017-08-17 12:13:42 +0300

    add functions

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,17 @@
 'use strict';
 
+// -----> Константы <-----
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var wizards = [];
+
+var amountWizards = 4;
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -15,40 +20,24 @@ var similarListElement = document.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var randomName = function () {
-  return WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + ' ' + WIZARD_LASTNAMES[Math.floor(Math.random() * WIZARD_LASTNAMES.length)];
+// -----> Получение рандомного индекса <-----
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
-
-var randomCoatColor = function () {
-  return COAT_COLOR[Math.floor(Math.random() * COAT_COLOR.length)];
+// -----> Получение рандомного значения <-----
+var getRandomValue = function (array) {
+  return array[getRandomInt(0, array.length - 1)];
 };
-
-var randomEyesColor = function () {
-  return EYES_COLOR[Math.floor(Math.random() * EYES_COLOR.length)];
+// -----> Создание мага <-----
+var createWizard = function () {
+  var wizard = {
+    name: getRandomValue(WIZARD_NAMES) + ' ' + getRandomValue(WIZARD_LASTNAMES),
+    coatColor: getRandomValue(COAT_COLORS),
+    eyesColor: getRandomValue(EYES_COLORS)
+  };
+  return wizard;
 };
-
-var wizards = [{
-  name: randomName(),
-  coatColor: randomCoatColor(),
-  eyesColor: randomEyesColor()
-},
-{
-  name: randomName(),
-  coatColor: randomCoatColor(),
-  eyesColor: randomEyesColor()
-},
-{
-  name: randomName(),
-  coatColor: randomCoatColor(),
-  eyesColor: randomEyesColor()
-},
-{
-  name: randomName(),
-  coatColor: randomCoatColor(),
-  eyesColor: randomEyesColor()
-}
-];
-
+// -----> Рендер мага <-----
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -58,13 +47,23 @@ var renderWizard = function (wizard) {
 
   return wizardElement;
 };
+// -----> Создание массива магов <-----
+var createWizards = function () {
+  for (var i = 0; i < amountWizards; i++) {
+    wizards[i] = createWizard();
+  }
+  return wizards;
+};
 
 var fragment = document.createDocumentFragment();
+// -----> Добавление мага в DOM <-----
+var pasteWizards = function () {
+  for (var j = 0; j < createWizards().length; j++) {
+    fragment.appendChild(renderWizard(wizards[j]));
+  }
+  return fragment;
+};
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-
-similarListElement.appendChild(fragment);
+similarListElement.appendChild(pasteWizards());
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
