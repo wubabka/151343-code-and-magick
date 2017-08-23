57fdec88c78862ba066312b2113a493b4185b923
wubabka <wubabca@gmail.comm>
2017-08-23 19:20:48 +0300

    Valid

--- js/setup.js
+++ js/setup.js
@@ -63,6 +63,33 @@ setupClose.addEventListener('keydown', function (evt) {
   }
 });
 
+// -----> Валидация форм <-----
+
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (!userNameInput.validity.valid) {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    }
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
 // -----> Рандомный цвет плаща по клику <-----
 wizardCoatChange.addEventListener('click', function () {
   wizardCoatChange.style.fill = getRandomValue(COAT_COLORS);
